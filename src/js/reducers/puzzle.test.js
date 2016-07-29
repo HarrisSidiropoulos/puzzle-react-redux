b1eae9e9e119ba2545b1ff3bbbf27f465eb0dd39
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-07-29 16:09:05 +0300

    clean up

--- src/js/reducers/puzzle.test.js
+++ src/js/reducers/puzzle.test.js
@@ -1,6 +1,11 @@
 import {expect} from 'chai';
 import puzzle, {INITIAL_STATE} from './puzzle'
 import {getPuzzlePartPosition, isPuzzleSolved, shufflePuzzle, PUZZLE_PART_POSITION, SHUFFLE_PUZZLE, IS_PUZZLE_SOLVED} from '../actions'
+
+const emptyPart = INITIAL_STATE.parts[11];
+const emptyPartX = emptyPart.x;
+const emptyPartY = emptyPart.y;
+
 describe('puzzle reducer', () => {
   describe(`${IS_PUZZLE_SOLVED} action`, () => {
     it('should be equal to initial state', () => {
@@ -12,6 +17,8 @@ describe('puzzle reducer', () => {
     it('should change a puzzle part position to empty part', () => {
       const state = puzzle(INITIAL_STATE, getPuzzlePartPosition(10))
       expect(state.parts[10].index).to.be.equal(11)
+      expect(state.parts[10].x).to.be.equal(emptyPartX)
+      expect(state.parts[10].y).to.be.equal(emptyPartY)
     });
   });
   describe(`${SHUFFLE_PUZZLE} action`, () => {
