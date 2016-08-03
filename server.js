bfdfdf4877e231d859399c85d3bfd2438b9482c1
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-08-03 20:24:46 +0300

    clean up

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /* eslint no-console: 0 */
 
 const path = require('path');
-const fs = require('fs');
 const express = require('express');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
