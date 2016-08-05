85d84dd668bab8676d14284c29823221cca66b99
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-08-05 20:51:31 +0300

    update tests for offlineProps

--- src/js/components/offline-update/offline-props.test.js
+++ src/js/components/offline-update/offline-props.test.js
@@ -2,10 +2,24 @@ import {expect} from 'chai'
 import offlineProps from './offline-props'
 
 describe('offlineProps', ()=> {
-  it('should return an object when enviroment is development', ()=> {
-    expect(offlineProps()).to.have.all.keys('install', 'applyUpdate', 'NODE_ENV')
+  describe('development', ()=> {
+    it('should return an object with keys: install, applyUpdate, NODE_ENV', ()=> {
+      expect(offlineProps()).to.have.all.keys('install', 'applyUpdate', 'NODE_ENV')
+    })
+    describe('install', ()=> {
+      it('should be a function', ()=> {
+        expect(offlineProps().install()).to.be.undefined
+      })
+    })
+    describe('applyUpdate', ()=> {
+      it('should be a function', ()=> {
+        expect(offlineProps().applyUpdate()).to.be.undefined
+      })
+    })
   })
-  it('should return an object when enviroment is production', ()=> {
-    expect(offlineProps('production')).to.have.all.keys('install', 'applyUpdate', 'NODE_ENV')
+  describe('production', ()=> {
+    it('should return an object with keys: install, applyUpdate, NODE_ENV', ()=> {
+      expect(offlineProps('production')).to.have.all.keys('install', 'applyUpdate', 'NODE_ENV')
+    })
   })
 })
