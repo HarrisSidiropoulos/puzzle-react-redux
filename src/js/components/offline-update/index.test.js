4c1594983ed0945f15430153751fb8cbd31a8b81
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-08-05 17:31:12 +0300

    update offline-update tests

--- src/js/components/offline-update/index.test.js
+++ src/js/components/offline-update/index.test.js
@@ -1,3 +1,4 @@
+/*eslint indent: */
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
 import chai, {expect} from 'chai'
@@ -5,13 +6,28 @@ import jsxChai from 'jsx-chai'
 chai.use(jsxChai)
 
 import OfflineUpdate from './index'
-function applyUpdate() {
+function applyUpdate() {}
 
-}
-function install() {
-
-}
-function getMainJSX(env='development') {
+function getMainJSX(env='development', state='') {
+  function install({onInstalled,onUpdating,onUpdateReady,onUpdateFailed,onUpdated}) {
+    switch(state) {
+      case 'onInstalled':
+        onInstalled()
+        break;
+      case 'onUpdating':
+        onUpdating()
+        break;
+      case 'onUpdateReady':
+        onUpdateReady()
+        break;
+      case 'onUpdateFailed':
+        onUpdateFailed()
+        break;
+      case 'onUpdated':
+        onUpdated()
+        break;
+    }
+  }
   const renderer = TestUtils.createRenderer()
   renderer.render(<OfflineUpdate NODE_ENV={env} install={install} applyUpdate={applyUpdate}/>)
   return renderer.getRenderOutput()
@@ -23,5 +39,30 @@ describe('OfflineUpdate', ()=> {
   })
   it('should have class hidden in production', ()=> {
     expect(getMainJSX('production')).to.include("hidden")
+  })
+  describe('onInstalled', ()=> {
+    it('should have class hidden', ()=> {
+      expect(getMainJSX('production', 'onInstalled')).to.include("hidden")
+    });
+  })
+  describe('onUpdating', ()=> {
+    it('should have class hidden', ()=> {
+      expect(getMainJSX('production', 'onUpdating')).to.include("hidden")
+    });
+  })
+  describe('onUpdateReady', ()=> {
+    it('should have class hidden', ()=> {
+      expect(getMainJSX('production', 'onUpdateReady')).to.include("hidden")
+    });
+  })
+  describe('onUpdateFailed', ()=> {
+    it('should have class hidden', ()=> {
+      expect(getMainJSX('production', 'onUpdateFailed')).to.include("hidden")
+    });
+  })
+  describe('onUpdated', ()=> {
+    it('should have class hidden', ()=> {
+      expect(getMainJSX('production', 'onUpdated')).to.include("hidden")
+    });
   })
 });
